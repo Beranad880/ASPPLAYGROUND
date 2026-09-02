aa4bfd1b2231c99f3a335826d3315d998419557a
adamh <adamhumble@outlook.cz>
2026-09-02 10:16:49 +0200

    Přidejte soubory projektu.

---
+++ App/Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationASP01.App
+{   
+
+    public class Controller: ControllerBase
+    {
+
+        [HttpGet("/ahoj")]
+        public IActionResult Index()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
+ 
